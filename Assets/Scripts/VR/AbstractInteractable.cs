a9233f8f1390b76db25a9425d3468460ac56076d
jesQM <jezuquema@gmail.com>
2020-11-04 15:41:05 +0100

    Implemented another gaze controller example

---
+++ Assets/Scripts/VR/AbstractInteractable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(GvrPointerGraphicRaycaster))]
+[RequireComponent(typeof(EventTrigger))]
+public abstract class AbstractInteractable : MonoBehaviour
+{
+    protected virtual void Start()
+    {
+        InitEventTrigger();
+    }
+
+    // Links the EventTrigger with the methods the methods that will be called
+    private void InitEventTrigger()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = null;
+
+        // Add click
+        entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerDown;
+        entry.callback.AddListener((data) => { OnPointerClick(); });
+        trigger.triggers.Add(entry);
+
+        // Add pointer enter
+        entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((data) => { OnPointerEnter(); });
+        trigger.triggers.Add(entry);
+
+        // Add pointer exit
+        entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerExit;
+        entry.callback.AddListener((data) => { OnPointerExit(); });
+        trigger.triggers.Add(entry);
+    }
+
+    public abstract void OnPointerClick();
+    public abstract void OnPointerEnter();
+    public abstract void OnPointerExit();
+}
---
+++ Assets/Scripts/VR/ColorChangerUsingInteractable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangerUsingInteractable : AbstractInteractable
+{
+    /*
+    // Si vamos a usar Start en la subclase, llamamos primero a la super, para que añada las llamadas a los métodos en el EventTrigger
+    protected override void Start()
+    {
+        base.Start();
+    }
+    */
+    
+
+    public override void OnPointerClick()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public override void OnPointerEnter()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public override void OnPointerExit()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
