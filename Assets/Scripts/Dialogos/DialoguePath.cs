b2437559be9d0b60673367f65afb7fb465fe72c3
jesQM <jezuquema@gmail.com>
2020-11-08 19:27:01 +0100

    Clases base de dialogo implementadas

--- Assets/Scripts/Dialogos/DialoguePath.cs
+++ Assets/Scripts/Dialogos/DialoguePath.cs
@@ -1,7 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 public class DialoguePath
 {
+    public Action OnPathTaken;
 
+    public string OptionName { get; private set; }
+    public Dialogue Destination { get; private set; }
+    
+    //
+    // Constructors
+    //
+    public DialoguePath(string optionName, Dialogue destination)
+    {
+        this.OptionName = optionName;
+        this.Destination = destination;
+    }
+
+    //
+    // Methods
+    //
+
+    public Dialogue TakePath()
+    {
+        OnPathTaken?.Invoke();
+        Destination?.OnNodeArrival?.Invoke();
+        return Destination;
+    }
 }
--- Assets/Scripts/Dialogos/DialogueTree.cs
+++ Assets/Scripts/Dialogos/DialogueTree.cs
@@ -1,7 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 public class DialogueTree
 {
+    public Dialogue Root { get; private set; }
+    public List<Dialogue> Dialogues { get; private set; } = new List<Dialogue>();
+    
+    //
+    // Constructors
+    //
 
+    public DialogueTree() { }
+
+    public DialogueTree(Dialogue root) : this() {
+        this.Root = root;
+        Dialogues.Add(root);
+    }
+
+    //
+    // Methods
+    //
+
+    public void AddDialogue(Dialogue dialogue)
+    {
+        if (Root == null) Root = dialogue;
+        Dialogues.Add(dialogue);
+    }
 }
