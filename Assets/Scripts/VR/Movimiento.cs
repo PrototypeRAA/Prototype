1def58494bf68dec780d19225eee2808995d69a2
raulng9 <salprez4@gmail.com>
2020-11-09 00:34:28 +0100

    Incluido movimiento básico al pulsar botón

---
+++ Assets/Scripts/VR/Movimiento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1")){
+            Debug.Log("Touch detected");
+            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+            Debug.Log(Camera.main.transform.forward);
+        }
+    }
+}
