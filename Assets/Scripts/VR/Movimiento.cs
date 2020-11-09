ca1f8d231ddaacea59ff9430186a5f41d6e0c204
raulng9 <salprez4@gmail.com>
2020-11-09 17:56:04 +0100

    Añadido bloqueo en el eje "y", translate y cambio de control de botón (movimiento continuo)

--- Assets/Scripts/VR/Movimiento.cs
+++ Assets/Scripts/VR/Movimiento.cs
@@ -6,18 +6,27 @@ public class Movimiento : MonoBehaviour
 {
     // Start is called before the first frame update
     public int playerSpeed;
+    private bool isMoving;
     void Start()
     {
-        
+        isMoving = false;
+        //transform.Translate(0f,1.5f,0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Fire1")){
-            Debug.Log("Touch detected");
-            transform.position = transform.position + Camera.main.transform.forward * playerSpeed * Time.deltaTime;
-            Debug.Log(Camera.main.transform.forward);
+        if(isMoving && !Input.GetButton("Fire1")){
+            //Sacamos los valores de la dirección de la cámara del jugador
+            float valueXAxis = Camera.main.transform.forward.x * Time.deltaTime * playerSpeed;
+            float valueZAxis = Camera.main.transform.forward.z * Time.deltaTime * playerSpeed;
+            //Movemos al jugador sin cambios en la altura
+            transform.Translate(valueXAxis, 0f, valueZAxis);
+            Debug.Log("Altura: " + transform.position.y);
+        } else if(Input.GetButton("Fire1") && !isMoving){
+            isMoving = true;
+        }else if (Input.GetButton("Fire1") && isMoving){
+            isMoving = false;
         }
     }
 }
