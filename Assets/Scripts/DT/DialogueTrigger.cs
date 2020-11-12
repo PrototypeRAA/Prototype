3e09e801669418f45c3ba04747bfa4a6237791f6
Tomás Argüelles <nekuyshiki@gmail.com>
2020-11-13 00:53:37 +0100

    Added animation
    Añadido Dialogo con sección de nombre y texto además de botón para pasar lineas. Viene con animaciones y botón de prueba

---
+++ Assets/Scripts/DT/DialogueTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue2 dialogue;
+
+    public void TriggerDialogue(){
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
---
+++ Assets/Scripts/OpenClose.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenClose : MonoBehaviour
+{
+    public Animator animator;
+    public void open(){
+        if(animator.GetBool("isOpen") == true){
+            animator.SetBool("isOpen", false);
+        }
+        else{
+            animator.SetBool("isOpen", true);
+        }
+    }
+}
