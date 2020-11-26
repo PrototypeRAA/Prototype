9f4a58431ee2c5dca3bb1446e92e5327c6de7df8
raulng9 <salprez4@gmail.com>
2020-11-27 00:47:30 +0100

    Añadidos el resto de colliders y lógica básica de pelotas para superar la sala 4

--- Assets/Scripts/VR/HoleForBallScript.cs
+++ Assets/Scripts/VR/HoleForBallScript.cs
@@ -13,22 +13,32 @@ public class HoleForBallScript : MonoBehaviour
 
     public GameObject puertaSala4;
 
+    public AudioSource audioPuerta;
+
+    public GameObject player;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
         isFilled = false;
         ownTag = gameObject.tag;
+        audioPuerta = puertaSala4.GetComponent<AudioSource>();
+
     }
 
      public void OnTriggerEnter(Collider col){
-       
-        if(col.tag == "BlueBall" && ownTag == "BlueHole"){
+        if(col.tag == "BlueBallS4" && ownTag == "BlueHole"){
             isFilled = true;
             CheckOtherHole();
         }
-        else if(col.tag == "RedBall" && ownTag == "RedHole"){
+        else if(col.tag == "RedBallS4" && ownTag == "RedHole"){
             isFilled = true;
             CheckOtherHole();
+        }
+        else if(col.tag == "Player"){
+            Debug.Log("game over");
         }
     }
 
@@ -41,7 +51,11 @@ public class HoleForBallScript : MonoBehaviour
     }
 
     public void AbrirPuertaSala4(){
-        
+        Debug.Log("Sala 4 superada");
+        puertaSala4.transform.Rotate(0, 0, 90);
+        Movimiento scriptMovimiento = player.GetComponent<Movimiento>();
+        scriptMovimiento.isMoving = false;
+        audioPuerta.Play();
     }
 
 }
