d3780673a569a6d5ec8d0ae69e9d519f8ba25190
raulng9 <salprez4@gmail.com>
2020-11-26 14:59:23 +0100

    Añadidas interacciones y lógica para superar la sala 3

--- Assets/Scripts/VR/HoleL3Script.cs
+++ Assets/Scripts/VR/HoleL3Script.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 public class HoleL3Script : MonoBehaviour
 {
     GameObject player;
-    // Start is called before the first frame update
+  
+
     void Start()
     {
         player = GameObject.FindWithTag("Player");
     }
 
     public void OnTriggerEnter(Collider col){
-    //Comprobar la tag del objeto con el que ha colisionado, si es el jugador, aplicar impulso
-    //para superar el foso y parar movimiento para el aterrizaje
-    if(col.tag == "Player"){
+    //Comprobar la tag del objeto, puede ser el jugador (game over) o el cubo (sala superada, abrir puerta)
+        if(col.tag == "Player"){
             Debug.Log("Game over");
         }
-
     }
 }
