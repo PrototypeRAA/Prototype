6ccfe13e8ee74d9c9dd6d46b68e54723e34f2701
jesQM <jezuquema@gmail.com>
2020-11-28 18:02:04 +0100

    La puerta inicial se abre tras completar el dialogo

--- Assets/Scripts/Dialogos/CreadorDialogos/CreadorDeDialogos.cs
+++ Assets/Scripts/Dialogos/CreadorDialogos/CreadorDeDialogos.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CreadorDeDialogos : MonoBehaviour
 {
     public DialogueCreator[] Dialogues;
     public DialogueTrigger Trigger;
     public DialogueTree Tree { get; private set; }
-    
+
     void Start()
     {
         Tree = new DialogueTree();
@@ -48,10 +49,25 @@ public struct DialogueCreator{
     public string[] OptionsText;
     public int[] OptionsDestination;
 
+    public UnityEvent[] OnNodeArrival;
+    public UnityEvent[] OnNodeLeave;
+
     public Dialogue ToDialogueWithoutOptions()
     {
+        // Como las lambda expression no pueden hacer referencia a this, se requieren estas variables locales
+        UnityEvent[] eventosOnArrival = OnNodeArrival;
+        UnityEvent[] eventosOnLeave = OnNodeLeave;
+
         Dialogue d = new Dialogue(this.Text, this.Speaker, this.Sprite);
         d.Sound = this.Sound;
+        d.OnNodeArrival += () => {
+            if (eventosOnArrival != null)
+                eventosOnArrival.ToList().ForEach(g => g.Invoke());
+        };
+        d.OnNodeLeave += () => {
+            if (eventosOnLeave != null)
+                eventosOnLeave.ToList().ForEach(g => g.Invoke());
+        };
         return d;
     }
 }
