86047967e4de1d5f4d010da73e28f0bba6d8ebeb
raulng9 <salprez4@gmail.com>
2020-12-04 01:19:15 +0100

    Añadida interacción básica de la sala final del nivel 1

---
+++ Assets/Scripts/VR/FinalRoomLevel1Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalRoomLevel1Script : AbstractInteractable
+{
+    
+    private bool isPlaying;
+
+    public AudioSource audioPortal;
+
+    private float growthRate;
+
+    void Start()
+    {
+        base.Start();
+        isPlaying = false;
+        growthRate = 0.01f;
+    }
+
+    void Update(){
+        if(isPlaying){
+        GrowPortal();
+        }
+    }
+
+
+    public override void HaSidoMirado()
+    {
+        if(!isPlaying){
+            audioPortal.PlayOneShot(audioPortal.clip);
+            isPlaying = true;
+        }
+    }
+
+    public void GrowPortal(){
+        this.gameObject.transform.localScale += new Vector3(this.gameObject.transform.localScale.x + 
+        growthRate,this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+    }
+
+    
+}
