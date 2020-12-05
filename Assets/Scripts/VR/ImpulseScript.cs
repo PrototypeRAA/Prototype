1db116a91fb71abfd7f903bee05fefe38214c18f
raulng9 <salprez4@gmail.com>
2020-12-06 00:49:28 +0100

    Añadidos elementos y lógica básica de la sala 2 (impulsos, colliders...)

---
+++ Assets/Scripts/VR/ImpulseScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseScript : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public void OnTriggerEnter(Collider col){
+        //Comprobar la tag del objeto con el que ha colisionado, si es el jugador, aplicar impulso,
+        //cambiar disposición del trampolín y reproducir sonidos asociados (despegue y aterizaje)
+        if(col.tag == "Player"){
+            float cameraValueX = Camera.main.transform.forward.x;
+            float cameraValueZ = Camera.main.transform.forward.z;
+            Vector3 springImpulse = new Vector3(cameraValueX, 5.0f, cameraValueZ);
+            Debug.Log(cameraValueX);
+            Debug.Log(cameraValueZ);
+            player.GetComponent<Rigidbody>().AddForce(springImpulse, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/VR/PlatformScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    public AudioSource landingSound;
+
+    public void OnTriggerEnter(Collider col){
+        if(col.tag == "Player"){
+            if(this.gameObject.tag == "FinalPlatform"){
+                GameObject player = GameObject.FindWithTag("Player");
+                GameObject doorRoom2Level2 = GameObject.FindWithTag("DoorRoom2Level2");
+                AudioSource audioPuerta = doorRoom2Level2.GetComponent<AudioSource>();
+                InventarioScript inventario = player.GetComponent<InventarioScript>();
+                inventario.CambiarTexto("Sala 2 superada!");
+                doorRoom2Level2.transform.Rotate(0, 0, -90);
+                Movimiento scriptMovimiento = player.GetComponent<Movimiento>();
+                scriptMovimiento.isMoving = false;
+                audioPuerta.PlayOneShot(audioPuerta.clip);
+            }
+            landingSound.Play();
+        }
+    }
+}
