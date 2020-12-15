5185328c19ac7b114449af865ff1379c21466e55
raulng9 <salprez4@gmail.com>
2020-12-15 14:07:24 +0100

    Revert "Merge branch 'master' into raul"
    This reverts commit 5acf3a3498edccde61576ef19994b13fcc8fb96b, reversing
    changes made to bd7ed39681ec9c29b32de8171b6d8ee15013df04.

--- Assets/Scripts/Dialogos/Dialogue.cs
+++ Assets/Scripts/Dialogos/Dialogue.cs
@@ -1,24 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Dialogue
 {
     public Action OnNodeArrival;
     public Action OnNodeLeave;
 
-
-    public string Speaker { get; set; }
-    public Sprite Sprite { get; set; }
     public string Text { get; private set; }
     public List<DialoguePath> Options { get; private set; } = new List<DialoguePath>();
-    public AudioClip Sound { get; set; }
-
-
-    /// Default Option goes to no dialogue => Ends conversation
-    private bool IsDefaultOptionIncluded = true;
-    private DialoguePath DefaultOption = new DialoguePath("default", null);
 
     //
     // Constructors
@@ -27,23 +17,11 @@ public class Dialogue
     public Dialogue(string text)
     {
         this.Text = text;
-        Options.Add(DefaultOption);
-    }
-
-    public Dialogue(string text, string speaker) : this(text)
-    {
-        this.Speaker = speaker;
-    }
-
-    public Dialogue(string text, string speaker, Sprite sprite) : this(text, speaker)
-    {
-        this.Sprite = sprite;
     }
 
     public Dialogue(string text, List<DialoguePath> options) : this(text)
     {
         Options = new List<DialoguePath>(options);
-        IsDefaultOptionIncluded = false;
     }
 
     //
@@ -52,7 +30,6 @@ public class Dialogue
 
     public void AddOption(DialoguePath option)
     {
-        if (IsDefaultOptionIncluded) IsDefaultOptionIncluded = !Options.Remove(DefaultOption); // Erase default option, no needed if we have one option
         Options.Add(option);
     }
 
